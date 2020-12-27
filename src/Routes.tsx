282d5998a50df7322405aec8f49f390efaf7cfea
Daniel Kim <danielkim107@gmail.com>
2020-12-27 20:00:28 +0900

    add routes for user

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import EntryForm from './components/EntryForm';
+import UserForm from './components/UserForm';
 
 const Routes = () => {
   return (
 	<Router>
 		<Switch>
-			<Route path="/:id" children={<EntryForm/>}/>
-			<Route path="/newEntry" children={<EntryForm/>}/>
+			<Route path="/entry/:id" children={<EntryForm/>}/>
+			<Route path="/entry/newEntry" children={<EntryForm/>}/>
+			<Route path="/user/newUser" children={<UserForm/>}/>
 			<Route path="/" component={Dashboard}/>
 		</Switch>
 	</Router>
