faa196a5af8b896bf3742fc378e51a03e3943022
Daniel Kim <danielkim107@gmail.com>
2020-12-29 12:30:51 +0900

    add login route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import EntryForm from './components/EntryForm';
+import LoginForm from './components/LoginForm';
 import UserForm from './components/UserForm';
 
 const Routes = () => {
@@ -11,6 +12,7 @@ const Routes = () => {
 			<Route path="/entry/:id" children={<EntryForm/>}/>
 			<Route path="/entry/newEntry" children={<EntryForm/>}/>
 			<Route path="/user/newUser" children={<UserForm/>}/>
+			<Route path="/login" children={<LoginForm/>}/>
 			<Route path="/" component={Dashboard}/>
 		</Switch>
 	</Router>
