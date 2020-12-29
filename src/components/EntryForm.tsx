b404483092ea48a29f9d0a9de4bc2503b33867cd
Daniel Kim <danielkim107@gmail.com>
2020-12-29 14:48:48 +0900

    remove userId and add authService

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -2,46 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
+import { AuthService } from '../utils/AuthService';
 
 const EntryForm = () => {
 	const history = useHistory();
 	const params: Param  = useParams();
 	const { register, handleSubmit, errors, reset } = useForm<EntryForm>();
-	const [ userList, setUserList ] = useState<Array<User>>([]);
+	const [ userId, setUserId ] = useState();
+	const authService = new AuthService();
 
 	useEffect(() => {
-		fetch('http://localhost:4000/user', {
-			method: 'GET',
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/json'
+		if (authService.checkLogin()) {
+			if (params && params.id !== 'newEntry') {
+				fetch(`http://localhost:4000/entry/${params.id}`, {
+					method: 'GET',
+					mode: 'cors',
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}).then(response => {
+					return response.json();
+				}).then(result => {
+					if (Object.keys(result).length === 0) {
+						alert('존재하지 않아, 접근할 수 없습니다.');
+						history.push('/dashboard');
+					} else {
+						reset(result);
+						setUserId(result.userId);
+					}
+				})
 			}
-		}).then(response => {
-			return response.json();
-		}).then(result => {
-			setUserList(result)
-		})
-		if (params && params.id !== 'newEntry') {
-			fetch(`http://localhost:4000/entry/${params.id}`, {
-				method: 'GET',
-				mode: 'cors',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}).then(response => {
-				return response.json();
-			}).then(result => {
-				if (Object.keys(result).length === 0) {
-					alert('존재하지 않아, 접근할 수 없습니다.');
-					history.push('/');
-				} else {
-					reset(result);
-				}
-			})
+		} else {
+			alert('로그인이 되어 있지 않습니다. 로그인 해주세요.');
+			history.push('/login');
 		}
 	}, []);
 
 	const onSubmit = (data: EntryForm) => {
+		let body:any = data;
+		body.userId = authService.getUser().id;
 		let method = 'POST';
 		let url = 'http://localhost:4000/entry';
 		if (params && params.id !== 'newEntry') {
@@ -56,7 +55,7 @@ const EntryForm = () => {
 			  'Content-Type': 'application/json'
 			},
 			referrerPolicy: 'no-referrer',
-			body: JSON.stringify(data)
+			body: JSON.stringify(body)
 		}).then(response => {
 			if (response.ok) {
 				alert('계시글 생성 완료');
@@ -64,45 +63,24 @@ const EntryForm = () => {
 				alert('실패');
 			}
 		}).finally(() => {
-			history.push('/');
+			history.push('/dashboard');
 		});
 	};
 
-	const deleteEntry = (id: string) => {
-		fetch(`http://localhost:4000/entry/${params.id}`, {
-			method: 'DELETE',
-			mode: 'cors'
-		}).then(response => {
-			if (response.ok && response.status === 204) {
-				alert('삭제 완료');
-				history.push('/');
-			} else {
-				alert('삭제 실패');
-			}
-		});
+	const checkUser = () => {
+		if (params.id !== 'newEntry') {
+			return authService.getUser().id	!== userId;
+		} else {
+			return false;
+		}
 	}
 
 	return (
 		<div className="container">
 			<Form onSubmit={handleSubmit(onSubmit)}>
-				<Form.Group controlId="userId">
-					<Form.Label>Author</Form.Label>
-					<Form.Control as="select" type="number" placeholder="Author" name="userId" custom ref={register({required: true})}>
-						{
-							userList.map((user) => {
-								return (<option key={user.id} value={user.id}>{user.username}</option>)
-							})
-						}
-					</Form.Control>
-					{errors.userId && errors.userId.type === "required" && (
-						<Form.Text className="text-danger">
-							You must choose an author.
-						</Form.Text>
-        			)}
-				</Form.Group>
 				<Form.Group controlId="title">
 					<Form.Label>Title</Form.Label>
-					<Form.Control type="text" placeholder="Title" name="title" maxLength={50} ref={register({required: true})}/>
+					<Form.Control type="text" placeholder="제목" name="title" maxLength={50} ref={register({required: true})} readOnly={checkUser()}/>
 					{errors.title && errors.title.type === "required" && (
 						<Form.Text className="text-danger">
 							You must enter the title.
@@ -111,17 +89,18 @@ const EntryForm = () => {
 				</Form.Group>
 				<Form.Group controlId="content">
 					<Form.Label>Content</Form.Label>
-					<Form.Control type="text" placeholder="Content" name="content" maxLength={500} ref={register({required: true})}/>
+					<Form.Control type="text" as="textarea" rows={6} placeholder="내용" name="content" maxLength={500} ref={register({required: true})} readOnly={checkUser()}/>
 					{errors.content && errors.content.type === "required" && (
 						<Form.Text className="text-danger">
 							You must enter some content.
 						</Form.Text>
         			)}
 				</Form.Group>
-				<Button variant="primary" type="submit">
-					Submit
-				</Button>
-				{params.id !== 'newEntry' && (<Button variant="danger" className="delete-button" onClick={() => deleteEntry(params.id)}>Delete</Button>)}
+				{(!checkUser() || params.id === 'newEntry') && (
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				)}
 			</Form>
 		</div>
 	);
