f3ebb4e9207df970d72bc99976caa100a6f4a5ad
Daniel Kim <danielkim107@gmail.com>
2020-12-30 14:27:27 +0900

    add api service

---
+++ src/api/api.ts
@@ -0,0 +1,39 @@
+const url = 'http://localhost:4000'
+
+export interface GetEntryDTO {
+	page: number;
+	limit: number;
+}
+
+export interface GetEntryResponse {
+	count: number;
+	data: Array<Entry>
+}
+
+// GetEntryQuery sends a request to server to fetch list of entries based on query
+export const GetEntryQuery = async (query: GetEntryDTO): Promise<GetEntryResponse> => {
+	const response = await fetch(`${url}/entry?limit=${query.limit}&page=${query.page}`, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+	})
+	const result = await response.json()
+	result.data.map((entry:Entry) => {
+		entry.createdAt = new Date(entry.createdAt).toLocaleDateString();
+		return entry
+	})
+	return result as GetEntryResponse
+		
+		
+		// .then(response => {
+		// 		return response.json();
+		// 	}).then(result => {
+		// 		let data = result.data.map((result: Entry) => {
+		// 			result.createdAt = new Date(result.createdAt).toLocaleDateString();
+		// 			return result;
+		// 		})
+				
+		// 	})
+}
