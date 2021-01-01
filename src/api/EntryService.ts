5b9e8f6a94c54745bd2669e6e64cedf17fcd23e5
Daniel Kim <danielkim107@gmail.com>
2021-01-01 17:17:10 +0900

    add API calls for Entry

--- src/api/EntryService.ts
+++ src/api/EntryService.ts
@@ -1,15 +1,5 @@
 import { API_URL } from '../constants';
 
-export interface GetEntryListDTO {
-	page: number;
-	limit: number;
-};
-
-export interface GetEntryListResponse {
-	data: Array<Entry>;
-	count: number;
-};
-
 // GetEntryQuery sends a request to server to fetch list of entries based on query.
 export const GetEntryList = async (query: GetEntryListDTO): Promise<GetEntryListResponse> => {
 	const response = await fetch(`${API_URL}/entry?limit=${query.limit}&page=${query.page}`, {
@@ -17,7 +7,7 @@ export const GetEntryList = async (query: GetEntryListDTO): Promise<GetEntryList
 			mode: 'cors',
 			headers: {
 				'Content-Type': 'application/json'
-			}
+			},
 	});
 	const result = await response.json();
 	result.data = result.data.map((entry:Entry) => {
@@ -28,9 +18,60 @@ export const GetEntryList = async (query: GetEntryListDTO): Promise<GetEntryList
 };
 
 // GetEntryById sends a request to server to fetch an entry based on PK id.
-export const GetEntryById = async (id: number): Promise<any> => {
+export const GetEntryById = async (id: string | number): Promise<GetEntryByIdResponse> => {
+	const response = await fetch(`${API_URL}/entry/${id}`, {
+		method: 'GET',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	});
+	const result = await response.json();
+	return result as GetEntryByIdResponse;
+};
 
-}
 // CreateEntry sends a request to server to create an entry based on the body.
+export const CreateEntry = async (body: CreateEntryDTO): Promise<Entry> => {
+	const response = await fetch(`${API_URL}/entry/`, {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		referrerPolicy: 'no-referrer',
+		body: JSON.stringify(body),
+	});
+	const result = await response.json();
+	return result as Entry;
+};
+
 // UpdateEntry sends a request to server to update an entry based on PK id.
+export const UpdateEntry = async (body: CreateEntryDTO, id: string | number): Promise<number> => {
+	const response = await fetch(`${API_URL}/entry/${id}`, {
+		method: 'PUT',
+		mode: 'cors',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		referrerPolicy: 'no-referrer',
+		body: JSON.stringify(body),
+	});
+	const result = await response.json();
+	return result as number;
+};
+
 // DeleteEntry sends a request to server to delete an entry based on PK id.
+export const DeleteEntry = async (id: string | number): Promise<void> => {
+	const response = await fetch(`${API_URL}/entry/${id}`, {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		referrerPolicy: 'no-referrer',
+	});
+	await response.json();
+};
