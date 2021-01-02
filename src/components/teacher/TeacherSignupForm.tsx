0ef08e36dc8107872d0be3451b8cfc6a3a3ca02e
Daniel Kim <danielkim107@gmail.com>
2021-01-02 14:49:42 +0900

    add updated path

--- src/components/teacher/TeacherSignupForm.tsx
+++ src/components/teacher/TeacherSignupForm.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
-import { SignupTeacher } from '../../api/TeacherService';
+import { SignupTeacher } from '../../api/teacher/TeacherService';
 
 const TeacherSignUpForm = () => {
 	const history = useHistory();
-	const { register, handleSubmit, errors } = useForm<UserForm>();
+	const { register, handleSubmit } = useForm<UserForm>();
 
 	const onSubmit = async (data: TeacherSignupFormDTO) => {
 		const response = await SignupTeacher(data);
@@ -25,29 +25,14 @@ const TeacherSignUpForm = () => {
 					<Form.Group controlId="username">
 						<Form.Label>아이디</Form.Label>
 						<Form.Control type="text" placeholder="아이디를 입력하세요" name="username" maxLength={50} ref={register({required: true})}/>
-						{errors.username && errors.username.type === "required" && (
-							<Form.Text className="text-danger">
-								아이디를 입력하세요.
-							</Form.Text>
-						)}
 					</Form.Group>
 					<Form.Group controlId="password">
 						<Form.Label>비밀번호</Form.Label>
 						<Form.Control type="password" placeholder="비밀번호를 입력하세요" name="password" maxLength={50} ref={register({required: true})}/>
-						{errors.password && errors.password.type === "required" && (
-							<Form.Text className="text-danger">
-								비밀번호를 입력하세요.
-							</Form.Text>
-						)}
 					</Form.Group>
 					<Form.Group controlId="name">
 						<Form.Label>이름</Form.Label>
 						<Form.Control type="text" placeholder="아이디를 입력하세요" name="name" maxLength={3} ref={register({required: true})}/>
-						{errors.username && errors.username.type === "required" && (
-							<Form.Text className="text-danger">
-								이름을 입력하세요.
-							</Form.Text>
-						)}
 					</Form.Group>
 					<Button variant="primary" type="submit">
 						등록
